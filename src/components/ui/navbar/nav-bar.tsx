737022ad73a32ceaa6cc795c09d078d672d88d52
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-11-24 23:57:09 +0300

    feat: add nav-bar component with user-info

---
+++ src/components/ui/navbar/nav-bar.tsx
@@ -0,0 +1,26 @@
+import { Button, Typography } from "@/components/ui";
+import { UserAvatar } from "@/components/ui/avatar/avatar";
+
+import s from "./nav-bar.module.scss";
+
+type NavBarPropsType = {
+  name: string;
+};
+
+export const NavBar = ({ name }: NavBarPropsType) => {
+  return (
+    <div className={s.navBar}>
+      <Typography variant={"body_2"}>Mini Chat</Typography>
+      <div className={s.user}>
+        <UserAvatar
+          name={name}
+          src={
+            "https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
+          }
+        />
+        <Typography variant={"body_2"}>{name}</Typography>
+        <Button variant={"link"}>Logout</Button>
+      </div>
+    </div>
+  );
+};
