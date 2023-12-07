671b5317011f0390a9a2f4a3519ffd56d418f5c9
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-12-07 23:57:33 +0300

    upd: refactored the search component and added search functionality

--- src/components/ui/search/search.tsx
+++ src/components/ui/search/search.tsx
@@ -1,30 +1,37 @@
 import { TextField, Typography } from "@/components/ui";
 import { UserAvatar } from "@/components/ui/avatar/avatar";
+import { UseSearch } from "@/services/auth/hooks/useSearch";
 
 import s from "./search.module.scss";
 
 type SearchPropsType = {
-  name: string;
+  name?: string;
 };
+
 export const Search = ({ name }: SearchPropsType) => {
+  const { handleKey, handleSelect, setUsername, user, username } = UseSearch();
+
   return (
     <div className={s.search}>
       <div className={s.searchForm}>
-        <TextField placeholder={"Find a user"} type={"search"} />
-      </div>
-      <div className={s.userChat}>
-        <UserAvatar
-          name={name}
-          src={
-            "https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
-          }
+        <TextField
+          onChange={(e) => setUsername(e.currentTarget.value)}
+          onKeyDown={handleKey}
+          placeholder={"Find a user"}
+          type={"search"}
+          value={username}
         />
-        <div className={s.userChatInfo}>
-          <Typography as={"span"} variant={"body_2"}>
-            {name}
-          </Typography>
-        </div>
       </div>
+      {user && (
+        <div className={s.userChat} onClick={handleSelect}>
+          <UserAvatar name={user.displayName} src={user.avatar} />
+          <div className={s.userChatInfo}>
+            <Typography as={"span"} variant={"body_2"}>
+              {user.displayName}
+            </Typography>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
