8e06535f1bf60a29cc0e05794551820f2d0b6b32
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-12-07 23:58:43 +0300

    upd: refactored sidebar component to dynamically load user chats

--- src/components/ui/sidebar/sidebar.tsx
+++ src/components/ui/sidebar/sidebar.tsx
@@ -1,18 +1,92 @@
+import { useEffect, useState } from "react";
+
 import { Chats } from "@/components/ui/chats/chats";
 import { NavBar } from "@/components/ui/navbar/nav-bar";
 import { Search } from "@/components/ui/search/search";
+import { auth, db } from "@/services/firebase/firebase";
+import { changeUser } from "@/services/slice/chat.slice";
+import { useAppDispatch } from "@/services/store/store";
+import { doc, onSnapshot } from "firebase/firestore";
 
 import s from "./sidebar.module.scss";
 
+interface Chat {
+  date: {
+    nanoseconds: number;
+    seconds: number;
+  };
+  lastMessage: string;
+  userInfo: {
+    displayName: string;
+    photoURL: string;
+    uid: string;
+  };
+}
+
+interface UserChats {
+  chats: { [key: string]: Chat };
+}
+
 export const Sidebar = () => {
+  const dispatch = useAppDispatch();
+  const [chats, setChats] = useState<UserChats>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const currentUser = auth.currentUser;
+
+    console.log("curUs", currentUser?.uid);
+
+    if (!currentUser) {
+      return () => {};
+    }
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChat", currentUser.uid), (doc) => {
+        console.log("Document data:", doc.data());
+        setChats(doc.data() as UserChats);
+        setLoading(false);
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+
+    currentUser.uid && getChats();
+  }, [auth.currentUser]);
+
+  if (loading) {
+    return <div>...Loading</div>;
+  }
+
+  const handleSelect = (uid: string, name: string) => {
+    const newUser = { displayName: name, uid: uid };
+
+    dispatch(changeUser({ user: newUser }));
+  };
+
+  const mappedChats = chats
+    ? Object.entries(chats).map((chat) => {
+        return (
+          <Chats
+            // avatar={chat[1].userInfo.photoURL}
+            handleSelect={() =>
+              handleSelect(chat[1].userInfo.uid, chat[1].userInfo.displayName)
+            }
+            key={chat[0]}
+            // message={chat[1].lastMessage?.text}
+            // name={chat[1].userInfo.displayName}
+            // uid={chat[1].userInfo.uid}
+          />
+        );
+      })
+    : [];
+
   return (
     <div className={s.sideBar}>
-      <NavBar name={"Vladimir"} />
+      <NavBar />
       <Search name={"Vladimir"} />
-      <Chats message={"Hello"} name={"Dimka Tractorist"} />
-      <Chats message={"Я удаляю все треки замая"} name={"Alexey fan Zamaya"} />
-      <Chats message={"Методы объекта"} name={"Vasya"} />
-      <Chats message={"C кем не бывает "} name={"Oleg"} />
+      <div>{mappedChats.length > 0 ? mappedChats : "No chats available"}</div>
     </div>
   );
 };
