31633fec5c205bb96f786df91ba479aa449f39f7
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-12-08 23:58:29 +0300

    upd: useSearch component has been updated with the addition of user selection and chat creation functionality

--- src/services/auth/hooks/useSearch.tsx
+++ src/services/auth/hooks/useSearch.tsx
@@ -1,32 +1,92 @@
-import { KeyboardEvent } from "react";
-import { useState } from "react";
+import { KeyboardEvent, useState } from "react";
 
-import { collection, getDocs, query, where } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 
-import { db } from "../../firebase/firebase"; // Замените на путь к вашему файлу конфигурации Firebase
+import { auth, db } from "../../firebase/firebase";
 
 export const UseSearch = () => {
   const [username, setUsername] = useState<string>("");
-  const [user, setUser] = useState<any | null>(null); // Используйте `any | null` для user, так как data может быть null
+  const [user, setUser] = useState<any | null>(null);
 
   const handleSearch = async () => {
     const q = query(
       collection(db, "users"),
       where("displayName", "==", username),
     );
-    const querySnapshot = await getDocs(q);
 
-    querySnapshot.forEach((doc) => {
-      setUser(doc.data());
-    });
+    try {
+      const querySnapshot = await getDocs(q);
+
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const handleKey = (e: KeyboardEvent<HTMLInputElement>) => {
     e.code === "Enter" && handleSearch();
   };
 
+  const handleSelect = async () => {
+    const currentUser = auth.currentUser;
+
+    if (!currentUser) {
+      return;
+    }
+    const combineId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+
+    try {
+      const res = await getDoc(doc(db, "chats", combineId));
+
+      if (!res.exists()) {
+        await setDoc(doc(db, "chats", combineId), { messages: [] });
+
+        await updateDoc(doc(db, "userChat", currentUser.uid), {
+          [combineId + ".date"]: serverTimestamp(),
+          [combineId + ".userInfo"]: {
+            displayName: user.displayName,
+            photoURL: user.photoURL || "",
+            uid: user.uid,
+          },
+        });
+
+        await updateDoc(doc(db, "userChat", user.uid), {
+          [combineId + ".date"]: serverTimestamp(),
+          [combineId + ".userInfo"]: {
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL || "",
+            uid: currentUser.uid,
+          },
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    setUser(null);
+    setUsername("");
+  };
+
   return {
     handleKey,
     handleSearch,
+    handleSelect,
+    setUsername,
+    user,
+    username,
   };
 };
