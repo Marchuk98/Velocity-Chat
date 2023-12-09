918891acff8a5fc4b3d8bfb3b8485a2d4453ee15
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-12-09 23:56:06 +0300

    upd: refactor nav-bar component using useRef and integrate useProfile hook

--- src/components/ui/navbar/nav-bar.tsx
+++ src/components/ui/navbar/nav-bar.tsx
@@ -1,28 +1,40 @@
+import { useRef } from "react";
+
 import { Button, Typography } from "@/components/ui";
 import { UserAvatar } from "@/components/ui/avatar/avatar";
+import { UseProfile } from "@/services/auth/hooks/useProfile";
 
 import s from "./nav-bar.module.scss";
 
-type NavBarPropsType = {
-  name: string;
-};
+export const NavBar = () => {
+  const fileRef = useRef<HTMLInputElement>(null);
 
-export const NavBar = ({ name }: NavBarPropsType) => {
+  const { avatar, handleFileChange, handleSignOut, username } = UseProfile();
+
   return (
     <div className={s.navBar}>
       <Typography className={s.logo} variant={"body_2"}>
         Mini Chat
       </Typography>
-      <div className={s.user}>
-        <UserAvatar
-          name={name}
-          src={
-            "https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
-          }
+      <UserAvatar
+        className={s.avatarWrapper}
+        name={username}
+        onClick={() => fileRef.current && fileRef.current.click()}
+        src={avatar}
+      />
+      <div className={s.avatarWrapper}>
+        <input
+          className={s.fileInput}
+          onChange={handleFileChange}
+          ref={fileRef}
+          style={{ display: "none" }}
+          type={"file"}
         />
-        <Typography variant={"body_2"}>{name}</Typography>
-        <Button variant={"link"}>Logout</Button>
       </div>
+      <Typography variant={"body_2"}>{username}</Typography>
+      <Button onClick={handleSignOut} variant={"link"}>
+        Logout
+      </Button>
     </div>
   );
 };
