1bd1943c62460ff839578905041c0e2d1d3b2aab
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-12-11 00:00:48 +0300

    upd: updated dialogs component to fetch messages from firestore collection

--- src/components/ui/dialogs/dialogs.tsx
+++ src/components/ui/dialogs/dialogs.tsx
@@ -1,18 +1,59 @@
+import { useEffect, useState } from "react";
+
+import { formatCustomDate } from "@/common/constants/formatCustomDate";
 import { Message } from "@/components/ui/dialogs/message/message";
+import { db } from "@/services/firebase/firebase";
+import { useAppSelector } from "@/services/store/store";
+import { Timestamp, doc, onSnapshot } from "firebase/firestore";
 
 import s from "./dialogs.module.scss";
 
+interface MessageArrInt {
+  date: Timestamp;
+  id: string;
+  img?: string;
+  senderId: string;
+  text: string;
+}
+
+interface ChatDialogs {
+  messages: MessageArrInt[];
+}
+
 export const Dialogs = () => {
+  // const [isLoading, setIsLoading] = useState(true);
+  const [messages, setMessages] = useState<ChatDialogs>({ messages: [] });
+  const chatId = useAppSelector((state) => state.chatId);
+
+  useEffect(() => {
+    if (!chatId) {
+      return;
+    }
+
+    const unSub = onSnapshot(doc(db, "chats", chatId), (doc) => {
+      doc.exists() && setMessages(doc.data() as ChatDialogs);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [chatId]);
+
+  const mappedDialogs = messages.messages?.map((m) => {
+    const formattedDate = m.date ? formatCustomDate(m.date.toDate()) : "";
+
+    return (
+      <Message
+        date={formattedDate}
+        img={m.img}
+        key={m.id}
+        senderId={m.senderId}
+        text={m.text}
+      />
+    );
+  });
+
   return (
-    <div className={s.dialogs}>
-      <Message info={"Just Now"} message={"hello"} name={"Vladimir"} />
-      <Message info={"1 hour ago"} message={"Buy"} name={"Alex"} />
-      <Message info={"Just Now"} message={"hello"} name={"Vladimir"} />
-      <Message info={"1 hour ago"} message={"Buy"} name={"Alex"} />
-      <Message info={"Just Now"} message={"hello"} name={"Vladimir"} />
-      <Message info={"1 hour ago"} message={"Buy"} name={"Alex"} />
-      <Message info={"Just Now"} message={"hello"} name={"Vladimir"} />
-      <Message info={"1 hour ago"} message={"Buy"} name={"Alex"} />
-    </div>
+    <div className={s.dialogs}>{mappedDialogs || "No dialogs available"}</div>
   );
 };
