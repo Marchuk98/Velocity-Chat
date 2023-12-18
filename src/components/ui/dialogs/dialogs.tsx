af295c6e159fc7fa17f427c393e5b86c5339a213
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-12-18 23:37:46 +0300

    upd: introduce isLoading state for displaying a loading indicator during data fetching. Check for the presence of messages before rendering, and show a corresponding message if none are available. Also, handle the case when no chat is selected.

--- src/components/ui/dialogs/dialogs.tsx
+++ src/components/ui/dialogs/dialogs.tsx
@@ -21,17 +21,21 @@ interface ChatDialogs {
 }
 
 export const Dialogs = () => {
-  // const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [messages, setMessages] = useState<ChatDialogs>({ messages: [] });
   const chatId = useAppSelector((state) => state.chatId);
 
   useEffect(() => {
+    setIsLoading(true);
     if (!chatId) {
+      setIsLoading(false);
+
       return;
     }
 
     const unSub = onSnapshot(doc(db, "chats", chatId), (doc) => {
       doc.exists() && setMessages(doc.data() as ChatDialogs);
+      setIsLoading(false);
     });
 
     return () => {
@@ -39,21 +43,36 @@ export const Dialogs = () => {
     };
   }, [chatId]);
 
-  const mappedDialogs = messages.messages?.map((m) => {
-    const formattedDate = m.date ? formatCustomDate(m.date.toDate()) : "";
-
-    return (
-      <Message
-        date={formattedDate}
-        img={m.img}
-        key={m.id}
-        senderId={m.senderId}
-        text={m.text}
-      />
-    );
-  });
+  const isChatSelected = !!chatId;
+  const hasMessages = messages.messages && messages.messages.length > 0;
 
   return (
-    <div className={s.dialogs}>{mappedDialogs || "No dialogs available"}</div>
+    <div className={s.dialogs}>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {isChatSelected ? (
+            hasMessages ? (
+              messages.messages?.map((m) => (
+                <Message
+                  date={formatCustomDate(m.date.toDate())}
+                  img={m.img}
+                  key={m.id}
+                  senderId={m.senderId}
+                  text={m.text}
+                />
+              ))
+            ) : (
+              <div className={s.dialogsChats}>
+                Поздоровайтесь с собеседником
+              </div>
+            )
+          ) : (
+            <div className={s.dialogsChats}>Выберите чат</div>
+          )}
+        </>
+      )}
+    </div>
   );
 };
