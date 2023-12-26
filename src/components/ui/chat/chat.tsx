f4621f8d5dac79fa716de72e576346ff67031cca
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-12-26 04:56:00 +0300

    upd: enhance сhat component with dynamic display

--- src/components/ui/chat/chat.tsx
+++ src/components/ui/chat/chat.tsx
@@ -1,29 +1,50 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
 import AddPerson from "@/assets/icons/Add-Person";
 import Camera from "@/assets/icons/Camera";
 import More from "@/assets/icons/More";
 import { Typography } from "@/components/ui";
 import { Dialogs } from "@/components/ui/dialogs/dialogs";
 import { EntryBlock } from "@/components/ui/entry-block/entry-block";
+import { RootState, useAppSelector } from "@/services/store/store";
 
 import s from "./chat.module.scss";
 
-type ChatPropsType = {
-  name: string;
-};
+export const Chat = () => {
+  const displayName = useSelector<RootState, string>(
+    (state) => state.user.displayName,
+  );
+  const chatId = useAppSelector((state) => state.chatId);
 
-export const Chat = ({ name }: ChatPropsType) => {
+  const [isChatSelected, setIsChatSelected] = useState(false);
+
+  useEffect(() => {
+    if (chatId) {
+      setIsChatSelected(true);
+    } else {
+      setIsChatSelected(false);
+    }
+  }, [chatId]);
+
   return (
     <div className={s.chat}>
-      <div className={s.chatInfo}>
-        <Typography variant={"body_2"}>{name}</Typography>
-        <div className={s.chatIcons}>
-          <Camera />
-          <AddPerson />
-          <More />
-        </div>
-      </div>
-      <Dialogs />
-      <EntryBlock />
+      {isChatSelected ? (
+        <>
+          <div className={s.chatInfo}>
+            <Typography variant={"h3"}>{displayName}</Typography>
+            <div className={s.chatIcons}>
+              <Camera />
+              <AddPerson />
+              <More />
+            </div>
+          </div>
+          <Dialogs />
+          <EntryBlock />
+        </>
+      ) : (
+        <div className={s.chatPlaceholder}>Выберите чат, чтобы продолжить</div>
+      )}
     </div>
   );
 };
--- src/services/slice/chat.slice.ts
+++ src/services/slice/chat.slice.ts
@@ -3,16 +3,20 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 type ChatInitialStateType = {
   chatId: null | string;
+  text: string;
   user: {
     displayName: string;
+    photoURL: string;
     uid: string;
   };
 };
 
 const initialState: ChatInitialStateType = {
   chatId: null,
+  text: "",
   user: {
     displayName: "",
+    photoURL: "",
     uid: "",
   },
 };
@@ -36,8 +40,11 @@ export const chatSlice = createSlice({
       }
       state.user = { ...user };
     },
+    setMessageText: (state, action: PayloadAction<string>) => {
+      state.text = action.payload;
+    },
   },
 });
 
-export const { changeUser } = chatSlice.actions;
+export const { changeUser, setMessageText } = chatSlice.actions;
 export const chatReducer = chatSlice.reducer;
