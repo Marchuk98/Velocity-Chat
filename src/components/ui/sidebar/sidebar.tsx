e12306f01e220a569511a9921d6fd6ccccaf7b3d
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-12-26 05:11:16 +0300

    upd: updated sidebar with improved logic and interface

--- src/components/ui/sidebar/sidebar.tsx
+++ src/components/ui/sidebar/sidebar.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+import { MainLoader } from "@/assets/loaders/main-loader";
 import { Chats } from "@/components/ui/chats/chats";
 import { NavBar } from "@/components/ui/navbar/nav-bar";
 import { Search } from "@/components/ui/search/search";
@@ -35,14 +36,13 @@ export const Sidebar = () => {
   useEffect(() => {
     const currentUser = auth.currentUser;
 
-    console.log("curUs", currentUser?.uid);
-
     if (!currentUser) {
+      setLoading(false);
+
       return () => {};
     }
     const getChats = () => {
       const unsub = onSnapshot(doc(db, "userChat", currentUser.uid), (doc) => {
-        console.log("Document data:", doc.data());
         setChats(doc.data() as UserChats);
         setLoading(false);
       });
@@ -56,36 +56,42 @@ export const Sidebar = () => {
   }, [auth.currentUser]);
 
   if (loading) {
-    return <div>...Loading</div>;
+    return <MainLoader />;
   }
 
-  const handleSelect = (uid: string, name: string) => {
-    const newUser = { displayName: name, uid: uid };
+  const handleSelect = (uid: string, name: string, photoURL: string) => {
+    const newUser = { displayName: name, photoURL, uid: uid };
 
     dispatch(changeUser({ user: newUser }));
   };
 
   const mappedChats = chats
-    ? Object.entries(chats).map((chat) => {
-        return (
-          <Chats
-            // avatar={chat[1].userInfo.photoURL}
-            handleSelect={() =>
-              handleSelect(chat[1].userInfo.uid, chat[1].userInfo.displayName)
-            }
-            key={chat[0]}
-            // message={chat[1].lastMessage?.text}
-            // name={chat[1].userInfo.displayName}
-            // uid={chat[1].userInfo.uid}
-          />
-        );
-      })
+    ? Object.entries(chats)
+        .sort((a, b) => b[1].date - a[1].date)
+        .map((chat) => {
+          return (
+            <Chats
+              avatar={chat[1].userInfo.photoURL}
+              handleSelect={() =>
+                handleSelect(
+                  chat[1].userInfo.uid,
+                  chat[1].userInfo.displayName,
+                  chat[1].userInfo.photoURL,
+                )
+              }
+              key={chat[0]}
+              message={chat[1].lastMessage?.text}
+              name={chat[1].userInfo.displayName}
+              uid={chat[1].userInfo.uid}
+            />
+          );
+        })
     : [];
 
   return (
     <div className={s.sideBar}>
       <NavBar />
-      <Search name={"Vladimir"} />
+      <Search />
       <div>{mappedChats.length > 0 ? mappedChats : "No chats available"}</div>
     </div>
   );
